63849d124fe645c658512ed31216dcf0c0d63207
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-07-15 18:21:13 +0200

    Connection to Database completed

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, function(){
-    console.log("Sizasneaks Backend is listening at Port: " + port);
-});
+//MongoDB Database Connection URL
+const MDBCONNECTURL =
+  "mongodb+srv://" + process.env.MONGODB_CONNECT_USERNAME +":" + process.env.MONGODB_CONNECT_PASSWORD + "@sizasana-sizzasneaks.wnfwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//Server Port Number.
+  const port = process.env.PORT || 5000;
+
+//Connect to MongoDB Server and Running Server.  
+mongoose
+  .connect(MDBCONNECTURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(function () {
+      //Running Server Only When MongoDB Connection Succesfully Made.
+    console.log("MongoDB Database Connected Successfully!");
+    app.listen(port, function () {
+      console.log("Sizzasneaks Server Running at Port: " + port);
+    });
+  })
+  .catch(function (error) {
+      //Outputing MongoDB Connection Error and Not Running Server.
+    console.log("MongoDB Database Connection Failed!");
+    console.log(error);
+  });
+
+//Link
+//mongodb+srv://coded19:<password>@sizasana-sizzasneaks.wnfwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
