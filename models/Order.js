07e4897e22710849cba732a14df7ef28833018cc
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-09-14 18:56:09 +0200

    SaveOrdersSchemas

--- models/Order.js
+++ models/Order.js
@@ -3,25 +3,39 @@ const OrderItem = require("./OrderItem.js");
 
 const Schema = mongoose.Schema; //Building a Mongoose Schema Object
 
-const OrderSchema = new Schema({
+const OrderSchema = new Schema(
+  {
+    customer_id: {
+      type: String,
+      required: true,
+    },
+    //shippingAddress: shippingAddress,
+    orderItems: { type: [String], required: true },
+    shippingCost: {
+      type: Number,
+      required: true,
+    },
 
-  _id: { type: String, unique: true },
- 
-  customer_id: String,
-  //shippingAddress:shippingAddress,
-  items:{type: [OrderItem]},
+    paymentComplete: {
+      type: Boolean,
+      required: true,
+    },
+    paymentTime: Date,
 
-  dateTime: { createdAt: "createdAt" },
+    hasShipped: {
+      type:Boolean,
+      required: true
+    },
+    shippedTime: Date,
 
-  shippingCost: Number,
-  
-  totalPaid: Number,
-  
-  totalTax: Number,
-  
-  status: String
- 
-});
+    hasBeenDelivered: {
+      type:Boolean,
+      required: true
+    },
+    deliveredTime: Date
+  },
+  { timestamps: { createdAt: "createdAt" } }
+);
 
 //Building a Model object from the Mongoose Schema Object.
 const OrderModel = mongoose.model("Order", OrderSchema);
--- models/OrderItem.js
+++ models/OrderItem.js
@@ -1,24 +1,53 @@
-const orderItem = {
-    _id: String,
-    
-    productId: String,
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const orderItem = new Schema(
+  {
+    productId: {
+      type: String,
+      required: true,
+    },
     option: {
-        //The color of the product option.
-        color: String,
-        //The size variant of the product option.
-        size: Number,
+      //The color of the product option.
+      color: {
+        type: String,
+        required: true,
       },
-    
-    saleDateTime: { createdAt: "createdAt" },
+      //The size variant of the product option.
+      size: {
+        type: Number,
+        required: true,
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    totalSupplierCost: {
+      type: Number,
+      required: true,
+    },
+    supplierTaxAmount: {
+      type: Number,
+      required: true,
+    },
+    sellingTaxAmount: {
+      type: Number,
+      required: true,
+    },
+    sellingPriceAmount: {
+      type: Number,
+      required: true,
+    },
+    orderItemCancelled: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: { createdAt: "createdAt" } }
+);
 
-    supplierTaxAmount: Number,
-
-    orderItemCost: Number,
-    
-    orderItemTaxAmount: Number,
-    
-    orderItemPrice: Number
-};
+//Building a Model object from the Mongoose Schema Object.
+const OrderItemModel = mongoose.model("OrderItem", orderItem);
 
 module.exports = orderItem;
