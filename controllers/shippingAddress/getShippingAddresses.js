dcea40cb964817a1f39eeb75e53d1429377d5d1f
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-09-17 23:53:09 +0200

    get Addresses Checked

--- controllers/shippingAddress/getShippingAddresses.js
+++ controllers/shippingAddress/getShippingAddresses.js
@@ -1,30 +1,25 @@
-const retrieveShippingAddresses = require("./functions/RetrieveShippingAddresses.js");
+const retrieveShippingAddresses = require("./functions/retrieveShippingAddresses.js");
 var { STATUS_CODE } = require("../constants/httpConstants.js");
 
 const getShippingAddresses = async function (req, res, next) {
   console.log("Get Shipping Addresses");
   try {
     if (req.body.credential === "customer") {
-      
       var shippingAddressesResult = await retrieveShippingAddresses(
-        req.body.userId);
+        req.body.userId
+      );
 
-        if (shippingAddressesResult.ok === true) {
-
-          res.status = STATUS_CODE.SUCCESS; 
-          res.send(shippingAddressesResult); 
-        } else {
-          res.status = STATUS_CODE.BAD_REQUEST;
-          res.send(shippingAddressesResult); 
-        }
-    }else{
+      if (shippingAddressesResult.ok === true) {
+        res.status = STATUS_CODE.SUCCESS;
+        res.send(shippingAddressesResult);
+      } else {
+        res.status = STATUS_CODE.BAD_REQUEST;
+        res.send(shippingAddressesResult);
+      }
+    } else {
       res.status = STATUS_CODE.UNAUTHORIZED;
-      res.send({ok:false, message:"Access Denied"})};
-      
-
-      
-      
-  
+      res.send({ ok: false, message: "Access Denied" });
+    }
   } catch (error) {
     console.log(error);
     res.status = STATUS_CODE.INTERNAL_SERVER_ERROR; //Attaches Internal Error Status Code to response object.
