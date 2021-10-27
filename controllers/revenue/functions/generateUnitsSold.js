d657f8ff017d24129ff5d0a46be967634d19a0fb
Lukumo Kajimanga <zaliwekajimanga@gmail.com>
2021-10-27 02:43:06 +0200

    Units Sold Function comments

--- controllers/revenue/functions/generateUnitsSold.js
+++ controllers/revenue/functions/generateUnitsSold.js
@@ -15,25 +15,32 @@ const monthNames = [
 
 async function generateUnitsSold(orderItems, sixMonthMark) {
   try {
-    
+    //creating collection of objects to hold groups of month and quantity from order items collection
     let orderItemObjects = {};
+
+    //loop through order item objects collection created to set month names, indexes and quantities
     for (let i = sixMonthMark; i <= (sixMonthMark + 5); i++) {
       orderItemObjects[monthNames[i]] = { month: i, quantity: 0 };
-	  console.log("I happened");
+	  
     }
 	console.log(orderItemObjects);
 
+    //loops through order items collection for month created  and quantity values
     for (var item of orderItems) {
+      //checks whether month and quantity have been defined
       if (
         typeof orderItemObjects[monthNames[item.createdAt.getMonth()]] !==
           "undefined" &&
         typeof orderItemObjects[monthNames[item.createdAt.getMonth()]]
           .quantity !== "undefined"
       ) {
+        //assigns month names to month values and increases the initial quantity value based on the values in the order item collection 
+        //saves assigned values to collection of order item objects
         orderItemObjects[monthNames[item.createdAt.getMonth()]].quantity +=
           item.quantity;
         
       } else {
+        //returns order item object collection  if month and quantity values are not defined
         orderItemObjects[monthNames[item.createdAt.getMonth()]] = {
           month: item.createdAt.getMonth(),
           quantity: item.quantity,
@@ -56,14 +63,5 @@ async function generateUnitsSold(orderItems, sixMonthMark) {
 }
 
 
-
-function updateUnitArrayItem(month, quantity, unitsArray) {
-  for (element of unitsArray) {
-    if (element.month === month) {
-      element.quantity = element.quantity + quantity;
-    }
-  }
-  return unitsArray;
-}
 
 module.exports = generateUnitsSold;
